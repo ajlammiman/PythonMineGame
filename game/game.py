c192e7604c3d049bf2ccc9f814b2f09f9414e324
Adam Lammiman <adam.lammiman@fdbhealth.com>
2023-01-30 17:47:59 +0000

    _

--- game/game.py
+++ game/game.py
@@ -5,16 +5,18 @@ class Game():
     ___output_text = '''Welcome to the Game, you must reach the other side. Try not to die! Press S to start.\n 
         The board is 8x8, you must cross to the other side but avoid the mines! 3 mines and you lose.\n
         move up with W, down with X, left with A and right with D.'''
+    ___start_text = "You have started on sqare {}:{}, what's your move?"
+    ___move_text = "You are now on square {}:{}, what's your move?"
         
     def __init__(self, start:dict = {"x":1, "y":1}) -> None:
         self.___move = MakeMove(start)
 
     def start(self):
-        self.___output_text = self.format_position("You have started on sqare {}:{}, what's your move?")
+        self.___output_text = self.format_position(self.___start_text)
     
     def player_move_up(self):
         self.___move.up()
-        self.___output_text = self.format_position("You are now on square {}:{}, what's your move?") 
+        self.___output_text = self.format_position(self.___move_text) 
 
     def player_move_right(self):
         self.___move.right()
