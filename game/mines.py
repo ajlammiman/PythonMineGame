66001d09ab02a60f9fc32df07cf21741eaf88368
Adam Lammiman <adam.lammiman@fdbhealth.com>
2023-02-18 18:37:05 +0000

    moved mocks to mock random numbers

--- game/mines.py
+++ game/mines.py
@@ -7,7 +7,7 @@ class Mines:
         self.___mines = self.__generate_mines(number_of_mines)
 
     def __generate_mines(self, number_of_mines: int):
-        return []
+        return [({"x": int(random.random()),"y": int(random.random())}) for _ in range(number_of_mines)]
     
     def check_for_mine(self, check_position:dict):
         return False
--- tests/test_mines.py
+++ tests/test_mines.py
@@ -1,25 +1,22 @@
 import unittest
 from unittest.mock import patch
-from game.board import Board
 from game.mines import Mines
 
 class test_mines(unittest.TestCase):
     
     def test_if_no_mines_defined_default_number_generated(self):
-        mines_mock = [{"x":1, "y":1},{"x":1, "y":2},{"x":1, "y":3},{"x":1, "y":4},{"x":1, "y":5},{"x":1, "y":6},{"x":1, "y":7},{"x":1, "y":8}]
+        mines = Mines()
+        self.assertEqual(len(mines._Mines___mines), 8)
 
-        with patch.object(Mines,"_Mines__generate_mines", lambda self, number_of_mines: mines_mock):     
-            mines = Mines()
-            self.assertEqual(len(mines._Mines___mines), 8)
-    
-    def test_number_of_mines_defined_are_same_as_generated(self):
-        number_of_mines = 9
-        mines_mock = [{"x":1, "y":1},{"x":1, "y":2},{"x":1, "y":3},{"x":1, "y":4},{"x":1, "y":5},{"x":1, "y":6},{"x":1, "y":7},{"x":1, "y":8},{"x":1, "y":9}]
 
-        with patch.object(Mines,"_Mines__generate_mines", lambda self, number_of_mines: mines_mock):     
-            mines = Mines(number_of_mines)
+    def test_mines_are_returned_as_a_list_of_coordinates_matching_expected_number_of_mines(self):
+        number_of_mines = 2
+        mined_locations = [{"x":1,"y":1},{"x":1,"y":1}]
+        with patch("game.mines.random.random", lambda : 1):     
+            mines = Mines(2)
             self.assertEqual(len(mines._Mines___mines), number_of_mines)
-        
+            self.assertEqual(mines._Mines___mines, mined_locations)
+    
     def test_informs_if_mine_is_present(self):
         mined_location = {"x":1,"y":1}
         number_of_mines = 1
